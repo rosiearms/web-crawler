118cf0e487a0ccd05e82b91722102a8d52f6608e
Rosie Armstrong <rosemary_a@live.co.uk>
2017-12-16 12:43:41 +0000

    add functions to organise results

--- crawler.js
+++ crawler.js
@@ -7,33 +7,61 @@ knwlInstance.register('phones', require('knwl.js/default_plugins/phones'));
 const request = require('request');
 const cheerio = require('cheerio');
 
-const emailAddress = 'tim@systemPioneer.com'
+const emailAddress = 'tim@kompli-global.com'
 const businessName = emailAddress.split(/\@(.*)/)[1];
 
 const visitPage = `http://www.${businessName}`;
 
 console.log('Scanning: ', visitPage);
 
-request(visitPage, function(error, response, HTML) {
+request(visitPage, function(error, response, body) {
   if(error) {
     console.log("Error: " + error);
   }
   if(response.statusCode === 200) {
-    var $ = cheerio.load(HTML);
-    var $body = $('body').text();
+    var $ = cheerio.load(body);
+    var $body = $('body').html();
   }
   
   
 knwlInstance.init($body);
 
-const phones = knwlInstance.get('phones');  
+const emails = knwlInstance.get('emails');
 const places = knwlInstance.get('places'); 
-const emails = knwlInstance.get('emails'); 
-const links = knwlInstance.get('links'); 
+const links = knwlInstance.get('links');
+const phones = knwlInstance.get('phones');  
 
-console.log('PHONE NUMBERS: ',phones);
-console.log('PLACES: ',places);
-console.log('EMAILS: ',emails);
-console.log('LINKS: ', links);
+function getEmails(emails) {
+  if (emails.length === 0) return [];
+  emails.forEach((email) => {
+    console.log('Email: ', email.address);
+  });
+}
+
+function getPhoneNumbers(phones) {
+  if (phones.length === 0) return [];
+  emails.forEach((phone) => {
+    console.log('Phone: ', phone.number);
+  });
+}
+
+function getPlaces(places) {
+  if (places.length === 0) return [];
+  places.forEach((place) => {
+    console.log('Place: ', place.place);
+  });
+}
+
+function getLinks(links) {
+  if (links.length === 0) return [];
+  links.forEach((link) => {
+    console.log('Link: ', link.link);
+  });
+}
+
+getEmails(emails);
+getPhoneNumbers(phones);
+getPlaces(places);
+getLinks(links);
 
 });
